f2225d3694b1b983d92ecc33ee99ccd39fee5fea
Martin Cutura <mcutura@student.42berlin.de>
2026-06-14 04:13:04 +0200

    fix: p2 pass server ip into the vm

--- p2/scripts/server_setup.sh
+++ p2/scripts/server_setup.sh
@@ -7,14 +7,10 @@ apt-get update
 apt-get install -y curl
 
 echo "[INFO] Installing K3s in server mode..."
-curl -sfL https://get.k3s.io | sh -s - server \
-	--write-kubeconfig-mode 600 \
-	--write-kubeconfig /home/vagrant/.kube/config
-
-echo "[INFO] Setting kubeconfig ownership..."
-chown vagrant:vagrant /home/vagrant/.kube/config
-
-echo "[INFO] K3s installation completed."
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
+  --node-ip ${SERVER_IP} \
+  --bind-address ${SERVER_IP} \
+  --write-kubeconfig-mode 644" sh -
 
 echo "[INFO] Waiting for K3s to be ready..."
 for i in {1..30}; do
@@ -29,12 +25,4 @@ done
 echo "[INFO] Checking K3s status..."
 systemctl status k3s --no-pager
 
-echo "[INFO] Installing kubectl..."
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-
-echo "[INFO] kubectl installation completed."
-
 echo "[INFO] Done. You can now run 'kubectl get nodes -o wide' after SSH into the VM."
-
