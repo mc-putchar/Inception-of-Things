7bf52ec8c4ac1ada2e0c7af95f7fd07372abb6f2
Martin Cutura <mcutura@student.42berlin.de>
2026-06-15 20:54:36 +0200

    fix(P3): add node port mapping into cluster creation

--- p3/scripts/deploy.sh
+++ p3/scripts/deploy.sh
@@ -2,7 +2,34 @@
 
 set -e
 
-k3d cluster create iot-cluster --api-port 6550 -p "8888:80@loadbalancer" --agents 1
+k3d cluster delete iot-cluster || true
+
+k3d cluster create iot-cluster \
+  --api-port 6550 \
+  --agents 1 \
+  -p "8888:30888@server:0" \
+  -p "8080:30080@server:0"
+
 kubectl apply -f ~/mnt/p3/confs/namespaces.yaml
-kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl apply -n argocd \
+  --server-side \
+  --force-conflicts \
+  -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd \
+    -p '{"spec": {"type": "NodePort", "ports": [{"port": 443, "nodePort": 30080}]}}'
+
 kubectl apply -f ~/mnt/p3/confs/argocd-app.yaml
+
+echo "Waiting for ArgoCD API to generate initial secrets..."
+sleep 10
+
+echo "================================================="
+echo "Access ArgoCD at: https://localhost:8080"
+echo "Access the Playground App at: http://localhost:8888"
+echo "================================================="
+echo "ArgoCD Login Credentials:"
+echo "Username: admin"
+echo -n "Password: "
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo;
+echo "================================================="
